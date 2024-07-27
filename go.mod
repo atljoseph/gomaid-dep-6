efbc75bf8b7fa481c49ca28eff0e7345da8468df
jgill <jgill@flourishsoftware.com>
2024-07-27 04:27:37 -0400

    fix mod deps

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/atljoseph/gomaid-dep-6
 
 go 1.22.1
+
+require github.com/atljoseph/gomaid-dep-5 v0.0.0-20240727082605-b47d84d18852
--- utils/hello.go
+++ utils/hello.go
@@ -1,7 +1,12 @@
 package utils
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/atljoseph/gomaid-dep-5/http"
+)
 
 func SayHelloFrom(name string) {
 	fmt.Printf("Hello from %s (w/ gomaid-deps-4)\n", name)
+	http.SayHello()
 }
